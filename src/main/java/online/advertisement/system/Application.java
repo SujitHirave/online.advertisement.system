d52494b28d4d528ce8ef9ad576b077f8081ef415
SujitHirave <hiravesujit11@gmail.com>
2021-07-09 14:19:14 +0530

    commit

--- src/main/java/online/advertisement/system/Application.java
+++ src/main/java/online/advertisement/system/Application.java
@@ -1,13 +1,26 @@
 package online.advertisement.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ 
+ *
+ */
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
+
+		LOG.info("Start");
 		SpringApplication.run(Application.class, args);
+		LOG.info("End");
 	}
 
 }
---
+++ src/main/java/online/advertisement/system/Hello.java
@@ -0,0 +1,31 @@
+package online.advertisement.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ 
+
+@RestController
+public class Hello {
+
+ 
+  Logger LOG= LoggerFactory.getLogger(Hello.class);
+  @RequestMapping("/")
+  public String welcome() {
+  	LOG.info("welcome");
+      return "welocome!";
+  }
+  
+  
+    @GetMapping("/hello")
+    public String hello() {
+    	LOG.info("Hello");
+        return "Hello world ok!";
+    }
+
+ 
+
+}

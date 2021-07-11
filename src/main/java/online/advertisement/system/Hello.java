1e806bb8367b14f8b0e4b8ca5927ad5cac7b6401
VenumadhavMamidala <venukanni38448@gmail.com>
2021-07-11 20:07:14 +0530

    commit

--- src/main/java/online/advertisement/system/Hello.java
+++ src/main/java/online/advertisement/system/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//venu
+
 @RestController
 public class Hello {
 
@@ -21,7 +21,7 @@ public class Hello {
   
     @GetMapping("/hello")
     public String hello() {
-    	LOG.info("Hello ak");
+    	LOG.info("Hello venu");
         return "Hello world ok!";
     }
 

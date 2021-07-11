b2378030b4ccf28ba186abf6183f8c8f3643a035
SujitHirave <hiravesujit11@gmail.com>
2021-07-11 11:04:59 +0530

    commit
    temp change

--- src/main/java/online/advertisement/system/Hello.java
+++ src/main/java/online/advertisement/system/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
- 
+ //test change by sujithirave temp
 
 @RestController
 public class Hello {

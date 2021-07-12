d201899c37c85d39c04b589ab335fc072ae83c0a
POOSATHE <sathepooja72@gmail.com>
2021-07-12 21:59:56 +0530

    commit

--- src/main/java/online/advertisement/system/model/Advertise.java
+++ src/main/java/online/advertisement/system/model/Advertise.java
@@ -1,5 +1,92 @@
 package online.advertisement.system.model;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "advertise_adv")
 public class Advertise {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String advertisetitle;
+	@Column(name = "price", nullable = false, length = 20)
+	private double price;
+	@Column(name = "description", nullable = false, length = 20)
+	private String description;
+	@Column(name = "status", nullable = false, length = 20)
+	private String status;
+
+	@ManyToOne
+	// @MapsId("catId")
+
+	@JoinColumn(name = "catid")
+	private Category category;
+
+	public Advertise() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Advertise(String advertisetitle, double price, String description, String status, Category category) {
+		super();
+		this.advertisetitle = advertisetitle;
+		this.price = price;
+		this.description = description;
+		this.status = status;
+		this.category = category;
+	}
+
+	public String getAdvertisetitle() {
+		return advertisetitle;
+	}
+
+	public void setAdvertisetitle(String advertisetitle) {
+		this.advertisetitle = advertisetitle;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Advertise [advertisetitle=" + advertisetitle + ", price=" + price + ", description=" + description
+				+ ", status=" + status + ", category=" + category + "]";
+	}
 
 }
---
+++ src/main/java/online/advertisement/system/model/Category.java
@@ -0,0 +1,63 @@
+package online.advertisement.system.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category_adv")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int catid;
+	@Column(name = "catname", nullable = false, length = 20)
+	private String catname;
+	@Column(name = "catdescription", nullable = false, length = 20)
+	private String catdescription;
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int catid, String catname, String catdescription) {
+		super();
+		this.catid = catid;
+		this.catname = catname;
+		this.catdescription = catdescription;
+	}
+
+	public int getCatid() {
+		return catid;
+	}
+
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+
+	public String getCatname() {
+		return catname;
+	}
+
+	public void setCatname(String catname) {
+		this.catname = catname;
+	}
+
+	public String getCatdescription() {
+		return catdescription;
+	}
+
+	public void setCatdescription(String catdescription) {
+		this.catdescription = catdescription;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [catid=" + catid + ", catname=" + catname + ", catdescription=" + catdescription + "]";
+	}
+
+}
